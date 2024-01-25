8d31a93e05f6e2acd85ee4a7a4389763dfac720f
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-01-25 23:59:37 +0530

    Updates

--- src/Three_Sum_Closest.java
+++ src/Three_Sum_Closest.java
@@ -1,23 +1,32 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class three_sum_functions{
-    int index_swap(int[] ar , int n , int target , int min){
-        for(int i = 0 ; i < 3 ; i ++){
-            int sum = 0 ;
-            for(int j = 0 ; j < 3 ; j ++){
-                if(i != j )
-                    sum += ar[j] ;
-                else
-                    sum += n ;
-            }
-            if(Math.abs(target - sum) <= min) {
-                min = Math.abs(target - sum);
-                ar[i] = n ;
-                break ;
+class Solution{
+    int threeSumClosest(int[] nums , int target){
+        int[] ar = new int[3] ;
+        ar[0] = nums[0] ;
+        ar[1] = nums[1] ;
+        ar[2] = nums[2] ;
+        Arrays.sort(ar);
+        int sum1 = ar[0] + ar[1] + ar[2] ;
+        int min = Math.abs(target - sum1) ;
+        for(int k = 3 ; k < nums.length ; k ++) {
+            for (int i = 0; i < 3; i++) {
+                int sum = 0;
+                for (int j = 0; j < 3; j++) {
+                    if (i != j)
+                        sum += ar[j];
+                    else
+                        sum += nums[k];
+                }
+                if (Math.abs(target - sum) <= min) {
+                    min = Math.abs(target - sum);
+                    ar[i] = nums[k];
+                    break;
+                }
             }
         }
-        return min ;
+        return (ar[0] + ar[1] + ar[2]) ;
     }
 }
 
@@ -34,18 +43,8 @@ public class Three_Sum_Closest {
         }
 //        System.out.println("Enter the integer number.");
         int target = sc.nextInt() ;
-        three_sum_functions ob = new three_sum_functions() ;
-        int[] ar = new int[3] ;
-        ar[0] = nums[0] ;
-        ar[1] = nums[1] ;
-        ar[2] = nums[2] ;
-        Arrays.sort(ar);
-//        Collections.reverse(Arrays.asList(ar));
-        int sum = ar[0] + ar[1] + ar[2] ;
-        int min = Math.abs(target - sum) ;
-        for(int i = 3 ; i < n ; i ++){
-            min = ob.index_swap(ar , nums[i] , target , min);
-        }
-        System.out.println(ar[0] + ar[1] + ar[2]);
+        Solution ob = new Solution() ;
+        int ret = ob.threeSumClosest(nums , target) ;
+        System.out.println(ret);
     }
 }
